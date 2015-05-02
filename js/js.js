ecb6474fbe97166ac0ef2c7b3e7d83e038321045
ricardocombat <europa>
2015-05-02 20:43:23 -0300

    multiples changes

---
+++ js/js.js
@@ -0,0 +1,38 @@
+$( document ).ready(function() {
+    //pega dadis
+    $.getJSON( "/data/dev.json", function( data ) {
+        var funcionarios = data.funcionarios;
+        
+        
+       //monta grid 
+        var x;
+        var items = [];
+        for (x in funcionarios) { 
+                items.push( "<li id='" + funcionarios[x].id + "'>" + funcionarios[x].nome + "</li>" );
+        }
+          $( "<ul/>", {
+                        "class": "lista-de-devs",html: items.join( "" )
+                      }).appendTo( "body" );
+
+    }); 
+    
+    //TODO CRIAR FORMA DE CONSUMIR E-MAIL E LISTAR PDA DOS USUÁRIOS QUE ESTÃO NA LISTA
+    
+    });
+
+
+
+   //add object
+   /* function createDev (){
+        var funcionario = {
+        "id" : $("#txtID").val(),
+        "nome" : $("#txtNOME").val(),
+        "thumb" : $("#txtTHUMB").val(),
+            };
+        funcionarios.push(funcionario); 
+        return true;   
+        
+        
+        $("#frmCadastre").bind("submit",function(){ 
+        return createDev(); 
+    } */
